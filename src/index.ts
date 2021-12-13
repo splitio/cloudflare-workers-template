b6e019bb10f94ad3402466c1b81dd16ca89a3ced
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-12-13 20:39:53 -0300

    feedback: extract request handlers into separate functions

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,48 @@ import { SplitStorageWrapper } from "./SplitStorageWrapper";
 // our Durable Object namespace, we must export it from the root module.
 export { SplitStorage } from "./SplitStorage";
 
+/**
+ * Worker
+ */
+export default {
+
+  // Handle HTTP incoming requests
+  async fetch(request: Request, env: Env) {
+    const url = new URL(request.url);
+
+    switch (url.pathname) {
+      // Use Split SDK to evaluate a feature. Request example `/get-treatment?key=some_key&split=some_split`
+      case "/get-treatment":
+        return handleGetTreatmentRequest(url, env);
+
+      // Synchronize your Split Storage, same as scheduled requests
+      case "/sync":
+        return handleSynchronization(env);
+
+      // Clean up all data from your Split Storage. SDK instances will evaluate to 'control'
+      case "/delete-all":
+        const durableObjectStub = getSplitStorage(env);
+        return durableObjectStub.fetch("https://dummy-url/deleteAll");
+
+      default:
+        return new Response("Not found", { status: 404 });
+    }
+  },
+
+  // Handle scheduled requests by a cron trigger, use to synchronize your Split Storage periodically
+  async scheduled(
+    controller: ScheduledController,
+    env: Env,
+    ctx: ExecutionContext
+  ) {
+    return handleSynchronization(env);
+  }
+};
+
+interface Env {
+  SplitStorage: DurableObjectNamespace;
+}
+
 // Server-side API key is required for Synchronizer and sufficient for JS Browser SDK
 const apiKey = "<YOUR SERVER-SIDE API KEY>";
 
@@ -21,8 +63,44 @@ function getSplitStorage(env: Env) {
   return env.SplitStorage.get(id);
 }
 
+// Use Split SDK to evaluate a feature
+async function handleGetTreatmentRequest(url: URL, env: Env) {
+  const key = url.searchParams.get("key");
+  if (!key) return new Response("No key provided", { status: 400 });
+
+  const split = url.searchParams.get("split");
+  if (!split) return new Response("No split provided", { status: 400 });
+
+  // SDK instances are created in 'consumer_partial' mode, which access
+  // the Split Storage to get the rollout plan data for evaluations
+  const factory = SplitFactory({
+    core: {
+      authorizationKey: apiKey,
+      key
+    },
+    mode: "consumer_partial",
+    storage: PluggableStorage({
+      wrapper: SplitStorageWrapper(getSplitStorage(env))
+    }),
+    // Disable or keep only ERROR log level in production, to minimize performance impact
+    debug: ErrorLogger()
+  });
+  const client = factory.client();
+
+  await client.ready();
+
+  // Async evaluation, because it access the rollout plan from the Split Storage
+  const treatment = await client.getTreatment(split);
+
+  // Flush data to Split backend. But not await, in order to reduce response latency
+  client.destroy();
+
+  // Do something with the treatment
+  return new Response(`Treatment: ${treatment}`);
+}
+
 // Synchronize rollout plan data into Split Storage durable object
-async function synchronizeSplitStorage(env: Env) {
+async function handleSynchronization(env: Env) {
   const synchronizer = new Synchronizer({
     core: {
       authorizationKey: apiKey
@@ -36,83 +114,5 @@ async function synchronizeSplitStorage(env: Env) {
   });
 
   await synchronizer.execute();
-}
-
-/**
- * Worker
- */
-export default {
-  // Handle user requests. Use Split SDK to evaluate features and track events
-  async fetch(request: Request, env: Env) {
-    const url = new URL(request.url);
-
-    // Request example `/get-treatment?key=some_key&split=some_split`
-    if (url.pathname === `/get-treatment`) {
-      const key = url.searchParams.get("key");
-      if (!key) return new Response("No key provided", { status: 400 });
-
-      const split = url.searchParams.get("split");
-      if (!split) return new Response("No split provided", { status: 400 });
-
-      // SDK instances are created in 'consumer_partial' mode, which access
-      // the Split Storage to get the rollout plan data for evaluations
-      const factory = SplitFactory({
-        core: {
-          authorizationKey: apiKey,
-          key
-        },
-        mode: "consumer_partial",
-        storage: PluggableStorage({
-          wrapper: SplitStorageWrapper(getSplitStorage(env))
-        }),
-        // Disable or keep only ERROR log level in production, to minimize performance impact
-        debug: ErrorLogger()
-      });
-      const client = factory.client();
-
-      await client.ready();
-
-      // Async evaluation, because it access the rollout plan from the Split Storage
-      const treatment = await client.getTreatment(split);
-
-      // Flush data to Split backend. But not await, in order to reduce response latency
-      client.destroy();
-
-      // Do something with the treatment
-      return new Response(`Treatment: ${treatment}`);
-    }
-
-    /** Control endpoints. Use them to synchronize and clean up the Split Storage */
-
-    // Requests to `/sync` will synchronize your Split Storage, same as cron trigger requests
-    if (url.pathname === "/sync") {
-      try {
-        await synchronizeSplitStorage(env);
-        return new Response("Synchronization success");
-      } catch (e) {
-        return new Response(`Synchronization fail: ${e}`);
-      }
-    }
-
-    // Requests to `/delete-all` will clean up all data from your Split Storage. SDK instances will evaluate to 'control'
-    if (url.pathname === "/delete-all") {
-      const durableObjectStub = getSplitStorage(env);
-      return durableObjectStub.fetch("https://dummy-url/deleteAll");
-    }
-
-    return new Response("Not found", { status: 404 });
-  },
-
-  // Scheduled requests by a cron trigger, use to synchronize your Split Storage periodically
-  async scheduled(
-    controller: ScheduledController,
-    env: Env,
-    ctx: ExecutionContext
-  ) {
-    await synchronizeSplitStorage(env);
-  }
-};
-
-interface Env {
-  SplitStorage: DurableObjectNamespace;
+  return new Response("Synchronization success");
 }
